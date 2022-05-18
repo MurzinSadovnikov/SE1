15354eb2c93563109cbca1a4601074d9cd0c4e11
MurzinSadovnikov <danila.murzin.98@mail.ru>
2022-05-18 20:31:59 +0500

    pechat raznosti

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int x, y, z, n;
-
+    cout << "Dannaja programma yavlyaetsya ychebnoy \n";
     cout << "Enter X: "; cin >> x;
     cout << "Enter Y: "; cin >> y;
     z=x+y;
