c0b923dbd738b813bb4a998806f4426a7ebb5557
MurzinSadovnikov <danila.murzin.98@mail.ru>
2022-05-18 17:22:49 +0500

    new ignore code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+
+    cout << "Enter X: "; cin >> x;
+    cout << "Enter Y: "; cin >> y;
+    z=x+y;
+    cout << "Sum=";
+    cout <<z;
+    return 0;
+}
