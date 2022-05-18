d608e0ed8e2fdb28e283527b41a29e8863322da3
MurzinSadovnikov <danila.murzin.98@mail.ru>
2022-05-18 21:43:31 +0500

    conflict

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
 //Учебная программа
+//Садовников Артем, Данила Мурзин, КЭ-317, 2022
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int x, y, z, n;
+    int x, y, z, n, d;
     cout << "Dannaja programma yavlyaetsya ychebnoy \n";
     cout << "Enter X: "; cin >> x;
     cout << "Enter Y: "; cin >> y;
     z=x+y;
     n=x-y;
+    d=x*y;
     cout << "Sum=";
     cout <<z;
     cout << "   Raznost'=";
     cout <<n;
+    cout << "   Proizvedenie'=";
+    cout <<d;
     return 0;
 }
